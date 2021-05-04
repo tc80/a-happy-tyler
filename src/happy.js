f2f53f452b45526282df41ff953649dc99176493
tallalnparis4ev <47358379+tallalnparis4ev@users.noreply.github.com>
2021-05-04 13:02:41 +0100

    Update src/happy.js
    Co-authored-by: Tyler Caslin <34184511+tc80@users.noreply.github.com>

--- src/happy.js
+++ src/happy.js
@@ -1,2 +1,2 @@
-//This is a program that doesn't take any input and logs the string literal "happy" to console
+// This is a program that doesn't take any input and logs the string literal "happy" to console.
 console.log("happy");
